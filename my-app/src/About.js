951bb82b2162fcd09eed136bde4dbc0f836c288f
Suresh B N <mesureshbn@gmail.com>
2022-04-11 15:11:52 -0500

    initial push

---
+++ my-app/src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1 className="title is-1">This is the About Page</h1>
+    <p>
+      Family details in here
+    </p>
+  </div>
+);
+
+export default About;
---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <header className="App-header">
+        <img src="https://rehabrocky.com/wp-content/uploads/2019/03/Family-Silhouette-3.jpg" />
+        <p>
+          Welcome to Family Page
+        </p>
+        <a>
+           Learn More About Family
+        </a>
+      </header>
+
+    </div>
+  );
+
+}
+
+export default App;
